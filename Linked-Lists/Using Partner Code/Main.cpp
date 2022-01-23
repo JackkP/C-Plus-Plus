347b7c632761d9b99dd5521d801b4c2306282cfe
Jack Porter <jackidporter@gmail.com>
2022-01-22 17:55:40 -0800

    finished Linked Lists Project

--- Linked-Lists/Using Partner Code/Main.cpp
+++ Linked-Lists/Using Partner Code/Main.cpp
@@ -20,18 +20,34 @@ void printForward(Node* n){ //function to print out students from the head to th
 	printForward(n->getNext()); //use recursion to print next student
 }
 
-bool searchID(Node* head, int ID){ //search for a student that matches this ID (used for avoiding duplicates)
-	Node* n = head;
-	while(n){ //search with iteration
-		if (n->getStudent()->getID() == ID){
-			return true;
-		}
-		n = n->getNext();
-	}
-	return false;
+float gpaSum(Node* n, float total){ //recursive function to return the summ of all student GPAs
+	if(!n) return total;
+	return gpaSum(n->getNext(), total+n->getStudent()->getGPA());
 }
 
-void addByID(Node*& head, Student* s){ //add a student in the correct place (in the linked list) by ID
+bool searchID(Node* n, int ID){ //recursively search for a student that matches this ID (used for avoiding duplicates)
+	if(!n) return false;
+	if (n->getStudent()->getID() == ID){
+		return true;
+	}
+	return searchID(n->getNext(), ID);
+}
+void insertAfter(Node* n, Node* a){ //recursively insert a somewhere after node n
+	//if n is the end of the list insert a
+	if(!n->getNext()){
+		n->setNext(a);
+		return;
+	}
+	//if the node after n is greater than A insert a after a
+	if(n->getNext()->getStudent()->getID() > a->getStudent()->getID()){ 	
+		a->setNext(n->getNext());
+		n->setNext(a);
+		return;
+	}
+	else insertAfter(n->getNext(), a);
+}
+
+void addByID(Node*& head, Student* s){ //add a student in the correct place (in the linked list) by ID	
 	Node* newNode = new Node(s); //create a new node
 	newNode->setNext(NULL); //set the node to default to null next pointer
 	if (!head) { head = newNode; return; } //if the head is a null pointer insert the student
@@ -42,23 +58,19 @@ void addByID(Node*& head, Student* s){ //add a student in the correct place (in 
 		head = newNode; //set the head to the new node
 		return;
 	}
-	
 	//if the student has a greater id than the head student
-	Node* n = head; //keep track of current node
-	while (n){ //search by iteration
-		//the id is greater than the last id value
-		if (!n->getNext() && n->getStudent()->getID() < s->getID()){
-			n->setNext(newNode);
-			return;
-		}
-		//the id is greater than the current id and smaller than the next
-		else if (s->getID() > n->getStudent()->getID() && s->getID() < n->getNext()->getStudent()->getID()){
-			newNode->setNext(n->getNext());
-			n->setNext(newNode);
-			return;
-		}
-		n = n->getNext();
+	insertAfter(head, newNode);
+}
+
+bool removeAfter(Node* n, int ID){ //recursively search for student to remove
+	if (!n->getNext()) return false; //if this is the end of the list return false
+	if (n->getNext()->getStudent()->getID() == ID){ //if the next node is the student remove it
+		Node* tmp = n->getNext()->getNext();
+		delete n->getNext();
+		n->setNext(tmp);
+		return true;
 	}
+	return removeAfter(n->getNext(), ID); //if there is a student it is somewhere after n->getNext()
 }
 
 bool deleteByID(Node*& head, int ID){ //delete a student given the ID
@@ -72,19 +84,7 @@ bool deleteByID(Node*& head, int ID){ //delete a student given the ID
 		head = tmp;
 		return true;
 	}
-	Node* n = head;
-	
-	//if(deleteByID(head->getNext(), ID)) return true;
-	while (n->getNext()){ //search by iteration
-		if(n->getNext()->getStudent()->getID() == ID){
-			Node* tmp = n->getNext()->getNext();
-			delete n->getNext();
-			n->setNext(tmp);
-			return true;
-		}
-		n = n->getNext();
-	}
-	return false;
+	return removeAfter(head, ID); //if there is a student, it is somewhere after the head
 }
 
 int main(){
@@ -101,7 +101,7 @@ int main(){
 		cout << "=====================================================" << endl;
 		cout << "Student Database: " << numStud << " students" << endl;
 
-		cout << "Type 'add' to add a student\n 'delete' to remove a student\n 'print' to list the students\n 'quit' to quit" << endl;
+		cout << "Type 'add' to add a student\n 'delete' to remove a student\n 'print' to list the students\n 'average' to print the average GPA\n 'quit' to quit" << endl;
 		
 		char next[100];
 		cin.get(next, 100);
@@ -154,7 +154,7 @@ int main(){
 			int id = 0;
 			cin >> id;
 			cin.get();
-			//if the delete completes remove one student from the ttracker
+			//if the delete completes remove one student from the tracker
 			if(deleteByID(head, id) == true) numStud --;
 			else cout << "that student does not exist" << endl;
 		}
@@ -164,6 +164,13 @@ int main(){
 			printForward(head);
 		}
 		
+		//print the average gpa
+		else if(strcmp(next, "average") == 0){
+			if(numStud != 0){
+				cout << "Average GPA: " << std::setprecision(2) << std::fixed << (gpaSum(head, 0)/numStud) << endl;
+			}
+			else cout << "No students to take the average of" << endl;
+		}
 		//quit the application
 		else if(strcmp(next, "quit") == 0){
 			cout << "quitting" << endl;
